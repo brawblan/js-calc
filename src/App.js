944f2ef22ff1d4c4d2ca8844591a7bf9ad436d25
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-02-11 21:44:42 -0600

    still getting evaluations working

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Display = () => {
-  const [displayState, setDisplayState] = useState(0);
-
-  const changeDisplay = ({ target: { value }}) => {
-    setDisplayState(value);
-  }
+const Display = ({ value }) => {
 
   return (
     <form className="display">
       <input 
         id="display"
         type="text" 
-        value={displayState} 
-        onChange={changeDisplay}
+        value={value} 
+        readOnly
       />
     </form>
   )
 }
 
-const Buttons = ({ id, value }) => {
+const Buttons = ({ id, value, onClick }) => {
+  const handleClick = () => onClick(value);
 
   return (
-    <div className="num-btn" id={id}>
+    <div 
+      className="num-btn" 
+      id={id}
+      onClick={handleClick}
+    >
       {value}
     </div>
   )
 }
 
-const ClearEquals = ({ id, value }) => {
+const ClearEquals = ({ id, value, onClick }) => {
+  const handleClick = () => onClick();
 
   return (
-    <div className={id} id={id}>
+    <div 
+      className={id} 
+      id={id}
+      onClick={handleClick}
+    >
       {value}
     </div>
   )
 }
 
 function App() {
+  const [displayState, setDisplayState] = useState([0]);
   const calcBtns = [
     { id: 'seven', value: 7 },
     { id: 'eight', value: 8 },
@@ -47,7 +53,7 @@ function App() {
     { id: 'four', value: 4 },
     { id: 'five', value: 5 },
     { id: 'six', value: 6 },
-    { id: 'multiply', value: 'x'},
+    { id: 'multiply', value: '*'},
     { id: 'one', value: 1 },
     { id: 'two', value: 2 },
     { id: 'three', value: 3 },
@@ -56,22 +62,41 @@ function App() {
     { id: 'decimal', value: '.' },
     { id: 'add', value: '+'},
   ]
-
   const clearEquals = [
     { id: 'clear', value: 'AC' },
     { id: 'equals', value: '=' },
   ]
+  const clickBtns = (btn) => {
+    console.log(displayState);
+    if (displayState[0] === 0) {
+      setDisplayState(btn)
+    } else {
+      setDisplayState([displayState, btn])
+    }
+  }
+  const onClear = () => {
+    setDisplayState([0])
+  }
+  const onEquals = (e) => {
+    setDisplayState(
+      String(eval(displayState).length > 3) &&
+        String(eval(displayState)).includes('.')
+        ? String(eval(displayState).toFixed(4))
+        : String(eval(displayState))
+    )
+  }
 
   return (
     <div className="calc-wrapper">
       JavaScript Calculator with React Hooks
-      <Display />
+      <Display value={displayState} />
       <div className="button-wrapper">
       {calcBtns.map((d) => (
         <Buttons 
-          id={d.id} 
-          value={d.value} 
-
+          id={d.id}
+          key={d.id}
+          value={d.value}
+          onClick={clickBtns}
         />
       ))}
       </div>
@@ -79,8 +104,8 @@ function App() {
         {clearEquals.map((d) => (
         <ClearEquals 
           id={d.id} 
-          value={d.value} 
-
+          value={d.value}
+          onClick={d.id === 'clear' ? onClear : onEquals}
         />
         ))}
       </div>
